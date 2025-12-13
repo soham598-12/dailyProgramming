09f74053922fb0669feaf830bae5a7ffb1f6a9e1
soham598-12 <sanchitapatel598@gmail.com>
2025-12-14 02:05:14 +0530

    day 12 update

---
+++ Day12/problem2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,s,r;
+    scanf("%d\n %d\n %d\n",&a,&s,&r);
+     if(a<30){
+        printf("High Risk Portfolio: Suitable for aggressive investors with high-risk tolerance.");
+    }
+    else if(s<=30000 && r<=2){
+        printf("Low Risk Portfolio: Suitable for conservative investments.");
+    }
+    else if(a>=30 && a<=50 && s>=75000 && r==3){
+        printf("High Risk Portfolio: Suitable for aggressive investors with high-risk tolerance.");
+    }
+    else if(a>50){
+        if(s>75000 && r==3){
+            printf("High Risk Portfolio: Suitable for aggressive investors with high-risk tolerance.");
+        }
+        else{
+            printf("Low Risk Portfolio: Suitable for conservative investments."); 
+        }
+    }
+    
+   else if(a>=30 && a<=50 && r<=2){
+        printf("Medium Risk Portfolio: Balanced risk for moderate returns.");
+    }
+    
+   
+    
+ 
+    return 0;
+}
